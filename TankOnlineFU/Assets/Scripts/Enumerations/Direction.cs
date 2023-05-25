dabe0b1411fe0a8519ce5d21e4a9f40b8769d438
datndw <datndw@gmail.com>
2023-05-25 21:42:52 +0700

    datnd create base user interfaces commit

--- TankOnlineFU/Assets/Scripts/Enumerations/Direction.cs
+++ TankOnlineFU/Assets/Scripts/Enumerations/Direction.cs
@@ -1,4 +1,4 @@
-namespace Entity
+namespace Enumerations
 {
     public enum Direction
     {
--- TankOnlineFU/Assets/Scripts/TankFirer.cs
+++ TankOnlineFU/Assets/Scripts/TankFirer.cs
@@ -1,5 +1,6 @@
 using System;
-using Entity;
+using Entities;
+using Enumerations;
 using UnityEngine;
 
 namespace DefaultNamespace
